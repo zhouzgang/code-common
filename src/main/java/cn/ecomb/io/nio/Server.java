91670ddd683043ee056c9e64164a7136d71d28e9
“zhouzg” <304118469@qq.com>
2018-04-02 02:00:40 +0800

    NIO demo

--- src/main/java/cn/ecomb/io/nio/Server.java
+++ src/main/java/cn/ecomb/io/nio/Server.java
@@ -5,4 +5,19 @@ package cn.ecomb.io.nio;
  * @date 2018/4/1.
  */
 public class Server {
+    private static int DEFAULT_PORT = 8080;
+    private static ServerHandle serverHandle;
+
+    public static void start() {
+        start(DEFAULT_PORT);
+    }
+
+    public static synchronized void start(int port) {
+
+    }
+
+    public static void main(String[] args) {
+        start();
+    }
+
 }
---
+++ src/main/java/cn/ecomb/io/nio/ServerHandle.java
@@ -0,0 +1,95 @@
+package cn.ecomb.io.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author zhouzhigang
+ * @date 2018/4/1.
+ */
+public class ServerHandle implements Runnable{
+
+    private Selector selector;
+    private ServerSocketChannel socketChannel;
+    private volatile boolean started;
+
+    public ServerHandle(int port) {
+        try {
+            selector = Selector.open();
+            socketChannel = ServerSocketChannel.open();
+
+            socketChannel.configureBlocking(false);
+            socketChannel.socket().bind(new InetSocketAddress(port), 1024);
+            socketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            started = true;
+            System.out.println("start successful, port is:" + port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    @Override
+    public void run() {
+        while (started) {
+            try {
+                selector.select(1000);
+                Set<SelectionKey> keys = selector.selectedKeys();
+                Iterator<SelectionKey> it = keys.iterator();
+                SelectionKey key = null;
+                while (it.hasNext()) {
+                    key = it.next();
+                    it.remove();
+                    handleInput(key);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (selector != null) {
+            try {
+                selector.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleInput(SelectionKey key) throws IOException{
+        if (key.isValid()) {
+            if (key.isAcceptable()) {
+                ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+                SocketChannel sc = ssc.accept();
+                sc.configureBlocking(false);
+                sc.register(selector, SelectionKey.OP_READ);
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
