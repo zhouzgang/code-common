4e6fb8304c38a7b0fd713b48b8e5e4f813f702d0
jonsnow.zhou <jonsnow.zhou@nf-3.com>
2019-01-24 10:31:19 +0800

    对象循环引用

---
+++ src/main/java/cn/ecomb/jvm/A.java
@@ -0,0 +1,13 @@
+package cn.ecomb.jvm;
+
+/**
+ * @author zhouzg
+ * @date 2019/1/23
+ */
+public class A {
+    B b = new B();
+
+    public static void main(String[] args) {
+        A a = new A();
+    }
+}
---
+++ src/main/java/cn/ecomb/jvm/B.java
@@ -0,0 +1,9 @@
+package cn.ecomb.jvm;
+
+/**
+ * @author zhouzg
+ * @date 2019/1/23
+ */
+public class B {
+    A a = new A();
+}
